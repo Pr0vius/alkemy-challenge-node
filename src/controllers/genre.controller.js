a7b029ff55da407748afbb1e015ce9bf91bd51c5
Braian <braiandelgado4255@hotmail.com>
2021-06-24 17:56:04 -0300

    Genre controller Initializated

---
+++ src/controllers/genre.controller.js
@@ -0,0 +1,76 @@
+const { Request, Response, NextFunction } = require("express");
+const ErrorResponse = require("../helpers/errorResponse");
+const Success = require("../helpers/successHandler");
+const Genre = require("../service/genre.service");
+
+/**
+ * Get Method
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+exports.getAllGenres = async (req, res, next) => {
+    try {
+        res.json(Success(await Genre.findAll()));
+    } catch (err) {
+        next(new ErrorResponse("ErrorMessage", 400, err));
+    }
+};
+
+/**
+ * Post Method
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+exports.createGenre = async (req, res, next) => {
+    const genre = ({} = req.body);
+    try {
+        res.json(Success(await Genre.create(genre)));
+    } catch (err) {
+        next(new ErrorResponse("ErrorMessage", 400, err));
+    }
+};
+
+/**
+ * Get Method
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+exports.getGenreById = async (req, res, next) => {
+    try {
+        res.json(Success(await Genre.findById(req.params.id)));
+    } catch (err) {
+        next(new ErrorResponse("ErrorMessage", 400, err));
+    }
+};
+
+/**
+ * Put Method
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+exports.updateGenre = async (req, res, next) => {
+    const genre = ({} = req.body);
+    try {
+        res.json(Success(await Genre.update(req.params.id, genre)));
+    } catch (err) {
+        next(new ErrorResponse("ErrorMessage", 400, err));
+    }
+};
+
+/**
+ * Delete Method
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+exports.deleteGenre = async (req, res, next) => {
+    try {
+        res.json(Success(await Genre.remove(req.params.id)));
+    } catch (err) {
+        next(new ErrorResponse("ErrorMessage", 400, err));
+    }
+};
