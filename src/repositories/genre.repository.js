8d52b9890e79a798158d3344962d85559754d8d9
Braian <braiandelgado4255@hotmail.com>
2021-06-24 17:50:44 -0300

    Genres repository initializated

---
+++ src/repositories/genre.repository.js
@@ -0,0 +1,30 @@
+const GenreSchema = require('../models/genre.schema');
+
+class GenreRepository{
+
+    constructor(){}
+
+    async findAll() {
+        return await GenreSchema.findAll()
+    }
+
+    async findById(id) {
+        return await GenreSchema.findByPk(id)
+    }
+
+    async create(genre){
+        return await GenreSchema.create(genre);
+    }
+
+    async update(id, genre){
+        GenreSchema.update(genre, {
+            where: { id: id },
+        });
+    }
+
+    async delete(id){
+        return await GenreSchema.destroy({where: {id}});
+    }
+}
+
+module.exports = GenreRepository
