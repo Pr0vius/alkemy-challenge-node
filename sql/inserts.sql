3bc45c5ff793c77376d033cac49552872d7c70f9
Braian <braiandelgado4255@hotmail.com>
2021-06-24 20:08:08 -0300

    SQL inserts

---
+++ sql/inserts.sql
@@ -0,0 +1,9 @@
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('ADVENTURE', 'https://m.media-amazon.com/images/M/MV5BMTYyMTcxNzc5M15BMl5BanBnXkFtZTgwOTg2ODE2MTI@._V1_.jpg');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('COMEDY', 'https://www.aceprensa.com/wp-content/uploads/2012/06/57800-0.jpg');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('ACTION', 'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/cinefilia/mejores-posters-no-oficiales-deadpool-2/deadpool2-09/137922903-1-esl-ES/deadpool2-09.jpg');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('SCIENCE_FICTION', 'http://d1f7geppf3ca7.cloudfront.net/origin/266782/tron_legacy_poster_1.jpg');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('MUSICAL', 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-8iil4l_d6ecf7bd.jpeg');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('FANTASY', 'https://i1.wp.com/hipertextual.com/wp-content/uploads/2020/09/hipertextual-avatar-2-logra-concluir-su-rodaje-pese-covid-19-2020475848.jpg?fit=1200%2C675&ssl=1');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('TERROR', 'https://i1.netflixmovies.com/dibsl9ebc/image/upload/w_1024/ovrxqsclq3wxhpqbgzeg.jpg');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('DRAMA', 'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/BF2990142FBFDE66B388DA85D7DB89E1E7C4CA2F75167A88D5DC689D2E37776D/scale?width=1200&aspectRatio=1.78&format=jpeg');
+INSERT INTO `0wKciVovPx`.`genres` (`name`, `image_url`) VALUES ('SUSPENSE', 'https://musicart.xboxlive.com/7/8ad72900-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080');
