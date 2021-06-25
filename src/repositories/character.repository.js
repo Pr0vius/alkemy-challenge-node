dd2c900a732bdd0cc86d3cdf5d16ad4e6fe3b426
Braian <braiandelgado4255@hotmail.com>
2021-06-24 22:02:59 -0300

    find by name function added

--- src/repositories/character.repository.js
+++ src/repositories/character.repository.js
@@ -1,30 +1,32 @@
-const CharacterSchema = require('../models/characters.schema');
+const CharacterSchema = require("../models/characters.schema");
 
-class CharacterRepository{
-
-    constructor(){}
+class CharacterRepository {
+    constructor() {}
 
     async findAll() {
-        return await CharacterSchema.findAll()
+        return await CharacterSchema.findAll();
     }
 
     async findById(id) {
-        return await CharacterSchema.findByPk(id)
+        return await CharacterSchema.findByPk(id);
     }
 
-    async create(character){
+    async findByName(name) {
+        return await CharacterSchema.findOne({ where: { name } });
+    }
+    async create(character) {
         return await CharacterSchema.create(character);
     }
 
-    async update(id, character){
+    async update(id, character) {
         CharacterSchema.update(character, {
             where: { id: id },
         });
     }
 
-    async delete(id){
-        return await CharacterSchema.destroy({where: {id}});
+    async delete(id) {
+        return await CharacterSchema.destroy({ where: { id } });
     }
 }
 
-module.exports = CharacterRepository
+module.exports = CharacterRepository;
